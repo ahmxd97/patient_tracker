1ffbd72566c3f19aa543cd5ccc5cce8576cde2b9
Muhammad Ahmed Arif <ah827867@gmail.com>
2025-12-09 00:59:55 +0500

    New Color 1

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'router.dart';
 void main() {
   runApp(
     ChangeNotifierProvider(
+      
       create: (_) => AuthProvider(),
       child: MaterialApp.router(
         routerConfig: router,
